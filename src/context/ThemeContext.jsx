439d0d973510bd08698a602c3df9659df3fab16a
Shubham Srivastava <121863397+sh2904ubham@users.noreply.github.com>
2025-03-14 10:58:50 +0530

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
